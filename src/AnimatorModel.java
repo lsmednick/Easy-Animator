1c53fe6cb050e41ec900d1b883a786ed1d7d3bb2
zhuje <59589720+zhuje@users.noreply.github.com>
2020-03-22 11:06:38 -0400

    update interface with methods 3/22/20 11:06

--- src/AnimatorModel.java
+++ src/AnimatorModel.java
@@ -1,6 +1,43 @@
 // Loge and Jenny love to code!
 
+import java.awt.*;
+
+/**
+ * TODO -- Interface for the Easy Animator Application.
+ * This supports an abstract class that that will define 2D shapes that are used for animation.
+ *
+ */
 public interface AnimatorModel {
+
+  /**
+   * Method to move shape.
+   * @param toX the X coordinate we're moving the shape To
+   * @param toY the Y coordinate we're moving the shape To
+   * @IllegalArgumentException throws if the parameters are outside of the spec
+   */
+   void move(double toX, double toY) throws IllegalArgumentException;
+
+   //TODO -- define the size of our 'display
+
+  /**
+   * Create a object that stores that values for RBG which will change the color of our Shape.
+   * @param myColor is the color we're trying to apply to our Shape
+   */
+  void changeColor(Color myColor);
+
+  /**
+   * Change the shape of our current Shape.
+   * @param myShape is the new shape we're changing the current Shape to
+   */
+  void changeShape (Shape myShape);
+
+  /**
+   * This method will scale the current shape.
+   * @param scalingLength scales the length by a given integer
+   * @param scalingWidth scales the width by a given interger
+   */
+  //TODO figure out SCALING
+  void scaling (int scalingLength, int scalingWidth);
 
 
 }
