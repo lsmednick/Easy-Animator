d06a56db56a364391be10dcebc773a173ba2092e
LoganMednick <mednick.l@husky.neu.edu>
2020-03-25 14:06:26 -0400

    3/22/20

--- src/AbstractShape.java
+++ src/AbstractShape.java
@@ -1,2 +1,148 @@
-public class AbstractShape implements IShape{
+import java.awt.Color;
+
+/**
+ * This class represents an abstract shape. It implements the IShape interface which includes
+ * various getter functions for the many attributes of an individual shape. This class is able to
+ * construct a new shape object with these parameters as well as return any the value of a given
+ * element.
+ */
+
+public class AbstractShape implements IShape {
+  private double x;
+  private double y;
+  private Color color;
+  private double width;
+  private double height;
+  private int startTime;
+  private int endTime;
+
+  /**
+   * This method constructs an abstract shape object. Each shape contains an x / y coordinate, rgb
+   * value, width, height, start time, and end time. While rgb values are supplied as integers,
+   * the constructor creates a Color object using this data in order to represent the given color.
+   *
+   * @param x x coordinate of the shape
+   * @param y y coordinate of the shape
+   * @param r red intensity of the shape's color
+   * @param g green intensity of the shape's color
+   * @param b blue intensity of the shape's color
+   * @param width width of the shape
+   * @param height height of the shape
+   * @param startTime starting time of the shape
+   * @param endTime end time of the shape
+   * @throws IllegalArgumentException if width or height is below 0
+   */
+
+  public AbstractShape(double x, double y, int r, int g, int b, double width, double height,
+                       int startTime, int endTime) throws IllegalArgumentException {
+    if (width <= 0 || height <= 0) {
+      throw new IllegalArgumentException("Width and height must be at least 0.");
+    }
+    this.x = x;
+    this.y = y;
+    this.color = new Color(r, g, b);
+    this.width = width;
+    this.height = height;
+    this.startTime = startTime;
+    this.endTime = endTime;
+  }
+
+  /**
+   * Method to return the x coordinate of a given shape.
+   *
+   * @return x coordinate of a shape.
+   */
+
+  @Override
+  public double getX() {
+    return this.x;
+  }
+
+  /**
+   * Method to return the y coordinate of a given shape.
+   *
+   * @return y coordinate of a shape.
+   */
+
+  @Override
+  public double getY() {
+    return this.y;
+  }
+
+  /**
+   * Method to return the red intensity of a shape's color.
+   *
+   * @return red intensity of a shape's color.
+   */
+
+  @Override
+  public int getR() {
+    return this.color.getRed();
+  }
+
+  /**
+   * Method to return the green intensity of a shape's color.
+   *
+   * @return green intensity of a shape's color.
+   */
+
+  @Override
+  public int getG() {
+    return this.color.getGreen();
+  }
+
+  /**
+   * Method to return the blue intensity of a shape's color.
+   *
+   * @return blue intensity of a shape's color.
+   */
+
+  @Override
+  public int getB() {
+    return this.color.getBlue();
+  }
+
+  /**
+   * Method to return the width of a given shape.
+   *
+   * @return width of a shape
+   */
+
+  @Override
+  public double getWidth() {
+    return this.width;
+  }
+
+  /**
+   * Method to return the height of a given shape.
+   *
+   * @return height of a shape
+   */
+
+  @Override
+  public double getHeight() {
+    return this.height;
+  }
+
+  /**
+   * Method to return the starting time of a given shape.
+   *
+   * @return starting time of a shape
+   */
+
+  @Override
+  public double getStartTime() {
+    return this.startTime;
+  }
+
+  /**
+   * Method to return the end time of a given shape.
+   *
+   * @return end time of a shape
+   */
+
+  @Override
+  public double getEndTime() {
+    return this.endTime;
+  }
 }
--- src/IShape.java
+++ src/IShape.java
@@ -1,2 +1,81 @@
+/**
+ * This is an interface for each of the shape classes. Each shape contains an x / y coordinate,
+ * rgb value, width, height, starting time, and end time. This interface contains getter methods
+ * for each of these attributes.
+ */
+
 public interface IShape {
+
+  /**
+   * Method to return the x coordinate of a given shape.
+   *
+   * @return x coordinate of a shape.
+   */
+
+  double getX();
+
+  /**
+   * Method to return the y coordinate of a given shape.
+   *
+   * @return y coordinate of a shape.
+   */
+
+  double getY();
+
+  /**
+   * Method to return the red intensity of a shape's color.
+   *
+   * @return red intensity of a shape's color.
+   */
+
+  int getR();
+
+  /**
+   * Method to return the green intensity of a shape's color.
+   *
+   * @return green intensity of a shape's color.
+   */
+
+  int getG();
+
+  /**
+   * Method to return the blue intensity of a shape's color.
+   *
+   * @return blue intensity of a shape's color.
+   */
+
+  int getB();
+
+  /**
+   * Method to return the width of a given shape.
+   *
+   * @return width of a shape
+   */
+
+  double getWidth();
+
+  /**
+   * Method to return the height of a given shape.
+   *
+   * @return height of a shape
+   */
+
+  double getHeight();
+
+  /**
+   * Method to return the starting time of a given shape.
+   *
+   * @return starting time of a shape
+   */
+
+  double getStartTime();
+
+  /**
+   * Method to return the end time of a given shape.
+   *
+   * @return end time of a shape
+   */
+
+  double getEndTime();
+
 }
