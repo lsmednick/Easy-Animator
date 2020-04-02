5db4810acf2ded307f327021d0b2b2c2fac4f31e
LoganMednick <mednick.l@husky.neu.edu>
2020-04-01 23:52:03 -0400

    4/1/20

--- src/cs5004/animator/view/IView.java
+++ src/cs5004/animator/view/IView.java
@@ -1,4 +1,40 @@
 package cs5004.animator.view;
 
+/**
+ * This is an interface to be implemented by each of our views.
+ */
+
 public interface IView {
+
+  /**
+   * Makes the view visible.
+   */
+
+  void makeVisible();
+
+  /**
+   * Sets the position of a given shape in order to be fed to the view.
+   *
+   * @param x the x coordinate of the shape
+   * @param y the y coordinate of the shape
+   */
+
+  void setShapePosition(double x, double y);
+
+  /**
+   * Writes out description of the animation to given text file name.
+   *
+   * @param name the name of the file to be populated with animation data
+   */
+
+  void output(String name);
+
+  /**
+   * Updates the view at each frame of the animation.
+   */
+
+  void refresh();
+
+
+
 }
---
+++ src/cs5004/animator/view/VisualView.java
@@ -0,0 +1,29 @@
+package cs5004.animator.view;
+
+import javax.swing.JFrame;
+
+/**
+ * This class represents a visual view of our EasyAnimator.
+ */
+
+public class VisualView extends JFrame implements IView {
+
+  @Override
+  public void makeVisible() {
+  }
+
+  @Override
+  public void setShapePosition(double x, double y) {
+
+  }
+
+  @Override
+  public void output(String name) {
+
+  }
+
+  @Override
+  public void refresh() {
+
+  }
+}
